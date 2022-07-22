d66b42830897d9f76960da1ff37559aed53325a4
Sarowar Saurav <64561923+SarowarSaurav@users.noreply.github.com>
2022-07-22 12:22:29 +0600

    update

--- client/src/routes/Pages/Home/Home.js
+++ client/src/routes/Pages/Home/Home.js
@@ -25,13 +25,13 @@ const Home = () => (
         </a>
         <a
           class="ph-mob"
-          href="https://www.producthunt.com/posts/blaze-2?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-blaze-2"
+          href="https://www.producthunt.com/posts/MetDrive-2?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-MetDrive-2"
           target="_blank"
           rel="noreferrer"
         >
           <img
             src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=174403&theme=dark&period=daily"
-            alt="Blaze - Fast peer to peer file sharing web app ⚡ | Product Hunt Embed"
+            alt="MetDrive - Fast peer to peer file sharing web app ⚡ | Product Hunt Embed"
             style="width: 250px; height: 54px;"
             width="250px"
             height="54px"
@@ -39,7 +39,7 @@ const Home = () => (
         </a>
       </div>
 
-      <img class="network-img" src={network} alt="Devices connected using Blaze" />
+      <img class="network-img" src={network} alt="Devices connected using MetDrive" />
 
       <svg class="waves" viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg">
         <path d="M0,160L21.8,160C43.6,160,87,160,131,181.3C174.5,203,218,245,262,234.7C305.5,224,349,160,393,133.3C436.4,107,480,117,524,106.7C567.3,96,611,64,655,80C698.2,96,742,160,785,176C829.1,192,873,160,916,138.7C960,117,1004,107,1047,112C1090.9,117,1135,139,1178,170.7C1221.8,203,1265,245,1309,234.7C1352.7,224,1396,160,1418,128L1440,96L1440" stroke-dasharray="1650" stroke-dashoffset="1650">
@@ -64,7 +64,7 @@ const Home = () => (
         <img src={twoDevices} alt="Laptop and mobile connected together" loading="lazy" style="margin-right: -60px" />
         <h2>Easy to use</h2>
         <p>
-          Blaze is a web app, just open it in your browser, join a room, and start sharing. No need to install any specific app, or create an account!
+          MetDrive is a web app, just open it in your browser, join a room, and start sharing. No need to install any specific app, or create an account!
         </p>
       </div>
 
@@ -72,15 +72,15 @@ const Home = () => (
         <img src={threeDevices} alt="Laptop and two mobiles connected together" loading="lazy" />
         <h2>Multi-device</h2>
         <p>
-          Traditionally, sharing files to multiple devices has been a hassle. With Blaze, you can share files across multiple devices with ease.
+          Traditionally, sharing files to multiple devices has been a hassle. With MetDrive, you can share files across multiple devices with ease.
         </p>
       </div>
       
       <div class="feature">
-        <img src={earth} alt="Devices in different parts of the world using Blaze to share files" loading="lazy" style="transform: scale(0.9)" />
+        <img src={earth} alt="Devices in different parts of the world using MetDrive to share files" loading="lazy" style="transform: scale(0.9)" />
         <h2>Anywhere</h2>
         <p>
-          Blaze is built on modern web technologies, allowing it to work on devices far away from each other. It just needs to be connected to the internet.
+          MetDrive is built on modern web technologies, allowing it to work on devices far away from each other. It just needs to be connected to the internet.
         </p>
       </div>
     </section>
@@ -88,15 +88,33 @@ const Home = () => (
     <section class="about" id="about">
 
       <blockquote>
-        <span>"</span>I built Blaze because I wanted a fast, radically different way to transfer files between my laptop and mobile<span>"</span>
+        <span></span>Demo Prototype for Battle of Minds 2022<span></span>
 
         <footer>
-          <a href="https://akashhamirwasia.com" target="_blank" rel="noopener noreferrer">
-            <img src="https://github.com/blenderskool.png?size=100" alt="Akash Hamirwasia" />
+          <a href="https://sarowarsaurav.com" target="_blank" rel="noopener noreferrer">
+            <img src="https://avatars.githubusercontent.com/u/64561923?v=4?size=200" alt="Sarowar" />
           </a>
-          <cite class="author">Akash Hamirwasia</cite>
-          <cite class="position">Creator of Blaze</cite>
+          <cite class="author">Sarowar Jahan</cite>
+          <cite class="position">Team Lead</cite>
         </footer>
+
+        <footer>
+          <a href="https://sarowarsaurav.com" target="_blank" rel="noopener noreferrer">
+            <img src="https://www.facebook.com/photo/?fbid=2196040523894306&set=a.104779956353717?size=200" alt="Ashik" />
+          </a>
+          <cite class="author">Ashikur Rahman</cite>
+          <cite class="position">Strategic Lead</cite>
+        </footer>
+
+        <footer>
+          <a href="https://github.com/SowatRafi" target="_blank" rel="noopener noreferrer">
+            <img src="https://avatars.githubusercontent.com/u/59786407?v=4?size=200" alt="Sowat" />
+          </a>
+          <cite class="author">Sowat Hossain Rafi</cite>
+          <cite class="position">Technical Lead</cite>
+        </footer>
+
+
       </blockquote>
 
     </section>
